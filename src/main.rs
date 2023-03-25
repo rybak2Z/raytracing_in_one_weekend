b1105410091cb23549ff553016bd6ffc2970504c
Felix Fischer <felixfischer0204@gmail.com>
2023-03-25 14:54:48 +0100

    Refactor into modules

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,5 @@
-use std::io::{self, BufWriter, StderrLock, StdoutLock, Write};
-
 use raytracing_in_one_weekend::vec3::Color;
+use raytracing_in_one_weekend::writing::*;
 
 const WIDTH: u32 = 256;
 const HEIGHT: u32 = 256;
@@ -13,29 +12,6 @@ fn main() -> io::Result<()> {
     render(&mut writer, &mut writer_err)?;
     finish_writers(&mut writer, &mut writer_err)?;
 
-    Ok(())
-}
-
-fn get_writers() -> (
-    BufWriter<StdoutLock<'static>>,
-    BufWriter<StderrLock<'static>>,
-) {
-    let stdout = io::stdout().lock();
-    let writer = BufWriter::new(stdout);
-
-    let stderr = io::stderr().lock();
-    let writer_err = BufWriter::new(stderr);
-
-    (writer, writer_err)
-}
-
-fn finish_writers(
-    writer: &mut BufWriter<StdoutLock>,
-    writer_err: &mut BufWriter<StderrLock>,
-) -> io::Result<()> {
-    writer.flush()?;
-    writer_err.write_all(b"\nDone.\n")?;
-    writer_err.flush()?;
     Ok(())
 }
 
@@ -54,26 +30,10 @@ fn render(
     Ok(())
 }
 
-fn write_progress_update(row: u32, writer_err: &mut BufWriter<StderrLock>) -> io::Result<()> {
-    write!(writer_err, "\rScanlines remaining: {row}")?;
-    writer_err.flush()?;
-    Ok(())
-}
-
 fn get_pixel_color(row: u32, col: u32) -> Color {
     Color::new(
         col as f64 / (WIDTH - 1) as f64,
         row as f64 / (HEIGHT - 1) as f64,
         0.25,
-    )
-}
-
-fn write_pixel<W: Write>(writer: &mut W, pixel_color: Color) -> io::Result<()> {
-    writeln!(
-        writer,
-        "{} {} {}",
-        (255.0 * pixel_color.x()) as u32,
-        (255.0 * pixel_color.y()) as u32,
-        (255.0 * pixel_color.z()) as u32
     )
 }
