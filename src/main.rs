c79c138f6a38a8cc06a21e6f30a827e20681c672
Felix Fischer <felixfischer0204@gmail.com>
2023-03-25 10:20:09 +0100

    Set up image writer
    Generate color gradient image and write to stdout. Run with "> image.ppm" to create .ppm file

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,33 @@
-fn main() {
-    println!("Hello, world!");
+use std::io::Write;
+use std::io::{self, BufWriter};
+
+const WIDTH: u32 = 256;
+const HEIGHT: u32 = 256;
+const FILE_TYPE: &str = "P3";
+const MAX_COLOR: u32 = 255;
+
+fn main() -> io::Result<()> {
+    let stdout = io::stdout().lock();
+    let mut writer = BufWriter::new(stdout);
+
+    writer.write_all(format!("{FILE_TYPE}\n{HEIGHT} {WIDTH}\n{MAX_COLOR}\n").as_bytes())?;
+
+    for row in (0..HEIGHT).rev() {
+        for col in 0..WIDTH {
+            let r = col as f32 / (WIDTH - 1) as f32;
+            let g = row as f32 / (HEIGHT - 1) as f32;
+            let b: f32 = 0.25;
+
+            let r = (255.0 * r) as u32;
+            let g = (255.0 * g) as u32;
+            let b = (255.0 * b) as u32;
+            
+            let output = format!("{r} {g} {b}\n");
+            writer.write_all(output.as_bytes())?;
+        }
+    }
+
+    writer.flush()?;
+
+    Ok(())
 }
