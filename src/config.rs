3e59bc712fd029e0a54b1df7655b7b38a479c810
rybak2Z <rybak2Z.000@gmail.com>
2023-03-26 20:06:43 +0200

    Enhance camera

--- src/config.rs
+++ src/config.rs
@@ -1,3 +1,5 @@
+use crate::vec3::Point3;
+
 // Image
 pub const ASCPECT_RATIO: f64 = 16.0 / 9.0;
 pub const IMAGE_WIDTH: u32 = 400;
@@ -9,6 +11,9 @@ pub const SAMPLES_PER_PIXEL: u32 = 100;
 pub const MAX_DEPTH: i32 = 50;
 
 // Camera
+pub const LOOK_FROM: Point3 = Point3::new(-2.0, 2.0, 1.0);
+pub const LOOK_AT: Point3 = Point3::new(0.0, 0.0, -1.0);
+pub const VIEW_UP: Point3 = Point3::new(0.0, 1.0, 0.0);
 pub const VERTICAL_FOV: f64 = 90.0;  // in degrees
 pub const FOCAL_LENGTH: f64 = 1.0;
 
