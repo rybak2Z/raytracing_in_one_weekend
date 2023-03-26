95f8f296793e1c842b990f634d3a4e391b7cb5ba
Felix Fischer <felixfischer0204@gmail.com>
2023-03-26 14:35:20 +0200

    Implement diffusion

--- src/config.rs
+++ src/config.rs
@@ -3,6 +3,7 @@ pub const ASCPECT_RATIO: f64 = 16.0 / 9.0;
 pub const IMAGE_WIDTH: u32 = 400;
 pub const IMAGE_HEIGHT: u32 = (IMAGE_WIDTH as f64 / ASCPECT_RATIO) as u32;
 pub const SAMPLES_PER_PIXEL: u32 = 100;
+pub const MAX_DEPTH: i32 = 50;
 pub const PROGRESS_NUM_WIDTH: u32 = IMAGE_HEIGHT.ilog10() + 1;
 
 // File format
