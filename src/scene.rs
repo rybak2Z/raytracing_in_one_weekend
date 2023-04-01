e0037bda52ab1103bf14007e41d96c674a7ebde4
rybak2Z <rybak2Z.000@gmail.com>
2023-04-01 18:22:56 +0200

    Implement validation

--- src/scene.rs
+++ src/scene.rs
@@ -1,11 +1,13 @@
 use crate::config::err_invalid_data;
-use crate::rendering::{HittableList, camera::*, Vec3, Point3, Color};
+use crate::rendering::{camera::*, Color, HittableList, Point3, Vec3};
 
 use serde::Deserialize;
 
 use std::io;
 
 const SCENE_PATH: &str = "default_scene.json";
+
+static mut DEFINED_MATERIALS: Vec<String> = Vec::new();
 
 #[derive(Deserialize)]
 struct Scene {
@@ -47,7 +49,22 @@ struct JsonCamera {
 
 impl JsonCamera {
     fn validate(&self) -> io::Result<()> {
-        todo!()
+        if self.vertical_fov_degrees <= 0.0 || self.vertical_fov_degrees >= 180.0 {
+            return Err(err_invalid_data("FOV must be between 0 and 180 degrees"));
+        }
+        if self.aperture <= 0.0 {
+            return Err(err_invalid_data("Aperture must be above 0"));
+        }
+        if let Some(distance) = self.focus_distance {
+            if distance < 0.0 {
+                return Err(err_invalid_data("Focus distance cannot be negative."));
+            }
+        }
+        if self.focal_length < 0.0 {
+            return Err(err_invalid_data("Focal length cannot be negative."));
+        }
+
+        Ok(())
     }
 }
 
@@ -59,7 +76,19 @@ enum JsonMaterial {
 
 impl JsonMaterial {
     fn validate(&self) -> io::Result<()> {
-        todo!()
+        if let Self::ReferenceToName(name) = self {
+            unsafe {
+                if DEFINED_MATERIALS.contains(name) {
+                    return Ok(());
+                } else {
+                    return Err(err_invalid_data(&format!("Undefined material: {name}")));
+                }
+            }
+        } else if let Self::Literal(literal) = self {
+            literal.validate()?;
+        }
+
+        Ok(())
     }
 }
 
@@ -69,12 +98,52 @@ struct JsonMaterialLiteral {
     type_: String,
     color: JsonColor,
     refractive_index: Option<f64>,
-    fuzziness: Option<f64>
+    fuzziness: Option<f64>,
 }
 
 impl JsonMaterialLiteral {
     fn validate(&self) -> io::Result<()> {
-        todo!()
+        if let Some(n) = &self.name {
+            unsafe {
+                DEFINED_MATERIALS.push(n.to_string());
+            }
+        }
+
+        match self.type_.as_str() {
+            "diffuse" => (),
+            "metal" => {
+                if let Some(fuzz) = self.fuzziness {
+                    if fuzz < 0.0 {
+                        return Err(err_invalid_data("Metal fuzziness cannot be negative"));
+                    }
+                } else {
+                    return Err(err_invalid_data(
+                        "Metal material needs the property \"fuzziness\".",
+                    ));
+                }
+            }
+            "dialectric" => {
+                if let Some(rf) = self.refractive_index {
+                    if rf < 0.0 {
+                        return Err(err_invalid_data("Refractive index cannot be negative."));
+                    }
+                } else {
+                    return Err(err_invalid_data(
+                        "Dialectric material needs the property \"refractive_index\"",
+                    ));
+                }
+            }
+            invalid_material => {
+                return Err(err_invalid_data(&format!(
+                    "Material type \"{}\" does not exist.",
+                    invalid_material
+                )));
+            }
+        }
+
+        self.color.validate()?;
+
+        Ok(())
     }
 }
 
@@ -86,7 +155,25 @@ struct JsonColor {
 
 impl JsonColor {
     fn validate(&self) -> io::Result<()> {
-        todo!()
+        let rgb = vec![self.rgb.0, self.rgb.1, self.rgb.2];
+        match self.normalized {
+            true => {
+                if rgb.iter().any(|num| *num < 0.0 || *num > 1.0) {
+                    return Err(err_invalid_data(
+                        "Normalized rgb values need to be between 0 and 1",
+                    ));
+                }
+            }
+            false => {
+                if rgb.iter().any(|num| *num < 0.0 || *num > 255.0) {
+                    return Err(err_invalid_data(
+                        "Non normalized rgb values need to be between 0 and 255",
+                    ));
+                }
+            }
+        }
+
+        Ok(())
     }
 }
 
@@ -100,7 +187,11 @@ struct JsonSphere {
 
 impl JsonSphere {
     fn validate(&self) -> io::Result<()> {
-        todo!()
+        if self.radius < 0.0 {
+            eprintln!("Warning: Sphere with negative radius created. This inverts the normals.");
+        }
+        self.material.validate()?;
+        Ok(())
     }
 }
 
@@ -117,7 +208,10 @@ fn read_scene_file() -> io::Result<Scene> {
     let file_contents = std::fs::read_to_string(SCENE_PATH)?;
     let scene = serde_json::from_str::<Scene>(&file_contents);
     if scene.is_err() {
-        return Err(err_invalid_data(&format!("Failed to deserialize {}", SCENE_PATH)));
+        return Err(err_invalid_data(&format!(
+            "Failed to deserialize {}",
+            SCENE_PATH
+        )));
     }
 
     Ok(scene.unwrap())
