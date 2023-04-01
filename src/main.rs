25ab1fca53a378e6ced2602ec67e8da99a2fb6ba
Felix Fischer <felixfischer0204@gmail.com>
2023-04-01 09:06:16 +0200

    Use new config statitcs in other modules

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use raytracing_in_one_weekend::config;
+use raytracing_in_one_weekend::config::*;
 use raytracing_in_one_weekend::rendering::{camera::Camera, render};
 use raytracing_in_one_weekend::world_building::*;
 use raytracing_in_one_weekend::writing::write_meta_data;
@@ -6,7 +6,12 @@ use raytracing_in_one_weekend::writing::write_meta_data;
 use std::io::{self, Error, ErrorKind};
 
 fn main() -> io::Result<()> {
-    if config::THREADS <= 0 {
+    let result = generate_config();
+    if let Err(e) = result {
+        print!("Error: {e}");
+    }
+
+    if *THREADS.get().unwrap() <= 0 {
         return Err(Error::new(
             ErrorKind::Other,
             format!("Invalid number of threads (needs to be greater or equal to 1)."),
@@ -15,15 +20,8 @@ fn main() -> io::Result<()> {
 
     write_meta_data()?;
 
-    let (world, world_cam_config) = match config::WORLD_TYPE {
-        WorldType::Custom1 => build_custom_1(),
-        WorldType::Random1 => build_random_1(),
-    };
-
-    let camera = match config::USE_WORLD_SPECIFIC_CAM {
-        true => Camera::new(world_cam_config),
-        false => Camera::new(config::CAMERA_CONFIG),
-    };
+    let (world, world_cam_config) = build_random_1();
+    let camera = Camera::new(world_cam_config);
 
     render(world, camera)?;
     eprintln!("\nDone.");
