f80df4009de5529bcff255f1397f5852629d7fc9
Felix Fischer <felixfischer0204@gmail.com>
2023-04-02 12:50:09 +0200

    Use new scene generation

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 use raytracing_in_one_weekend::config::*;
-use raytracing_in_one_weekend::rendering::{camera::Camera, render};
-use raytracing_in_one_weekend::world_building::*;
+use raytracing_in_one_weekend::rendering::render;
+use raytracing_in_one_weekend::scene::generate_scene;
 use raytracing_in_one_weekend::writing::write_meta_data;
 
 use std::io::{self, Error, ErrorKind};
@@ -20,8 +20,7 @@ fn main() -> io::Result<()> {
 
     write_meta_data()?;
 
-    let (world, world_cam_config) = build_random_1();
-    let camera = Camera::new(world_cam_config);
+    let (world, camera) = generate_scene()?;
 
     render(world, camera)?;
     eprintln!("\nDone.");
