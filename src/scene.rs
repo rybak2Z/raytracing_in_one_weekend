b288d628803f310544405fb209ecd649b3916b7e
Felix Fischer <felixfischer0204@gmail.com>
2023-04-02 12:47:32 +0200

    Finish scene generation

--- src/scene.rs
+++ src/scene.rs
@@ -1,7 +1,6 @@
 mod validation;
 
-use crate::config::err_invalid_data;
-use crate::rendering::{camera::*, Color, HittableList, Point3, Vec3};
+use crate::rendering::{camera::*, HittableList, Point3, Vec3};
 
 use serde::Deserialize;
 
@@ -35,6 +34,7 @@ struct JsonCamera {
 }
 
 #[derive(Deserialize)]
+#[serde(untagged)]
 enum JsonMaterial {
     ReferenceToName(String),
     Literal(JsonMaterialLiteral),
@@ -44,7 +44,7 @@ enum JsonMaterial {
 struct JsonMaterialLiteral {
     name: Option<String>,
     type_: String,
-    color: JsonColor,
+    color: Option<JsonColor>,
     refractive_index: Option<f64>,
     fuzziness: Option<f64>,
 }
@@ -66,29 +66,44 @@ struct JsonSphere {
 pub fn generate_scene() -> io::Result<(HittableList, Camera)> {
     let scene = read_scene_file()?;
     scene.validate()?;
-    let camera = create_camera(scene.camera)?;
-    let world = create_world(scene.objects)?;
+    let camera = create_camera(scene.camera);
+    let world = create_world(scene.objects);
 
     Ok((world, camera))
 }
 
 fn read_scene_file() -> io::Result<Scene> {
     let file_contents = std::fs::read_to_string(SCENE_PATH)?;
-    let scene = serde_json::from_str::<Scene>(&file_contents);
-    if scene.is_err() {
-        return Err(err_invalid_data(&format!(
-            "Failed to deserialize {}",
-            SCENE_PATH
-        )));
+    let scene = serde_json::from_str::<Scene>(&file_contents)?;
+
+    Ok(scene)
+}
+
+fn create_camera(json_camera: JsonCamera) -> Camera {
+    let coords = json_camera.look_from;
+    let look_from = Point3::new(coords.x, coords.y, coords.z);
+    let coords = json_camera.look_at;
+    let look_at = Point3::new(coords.x, coords.y, coords.z);
+    let vec = json_camera.view_up_direction;
+    let view_up = Vec3::new(vec.x, vec.y, vec.z);
+
+    let camera_config = CameraConfiguration {
+        look_from,
+        look_at,
+        view_up,
+        vertical_fov: json_camera.vertical_fov_degrees,
+        aperture: json_camera.aperture,
+        focus_distance: json_camera.focus_distance,
+        focal_length: json_camera.focal_length,
+    };
+
+    Camera::new(camera_config)
+}
+
+fn create_world(json_objects: Vec<JsonSphere>) -> HittableList {
+    let mut world = HittableList::default();
+    for json_sphere in json_objects {
+        world.add(Box::new(json_sphere.to_sphere()));
     }
-
-    Ok(scene.unwrap())
-}
-
-fn create_camera(json_camera: JsonCamera) -> io::Result<Camera> {
-    todo!()
-}
-
-fn create_world(json_objects: Vec<JsonSphere>) -> io::Result<HittableList> {
-    todo!()
+    world
 }
