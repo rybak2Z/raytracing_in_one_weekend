e012756038f0bfb27cc4705e8c23439efed26024
Felix Fischer <felixfischer0204@gmail.com>
2024-03-28 18:12:58 +0100

    Add re-exports in lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -2,6 +2,10 @@ pub mod color;
 pub mod point3;
 pub mod vec3;
 
+pub use color::Color;
+pub use point3::Point3;
+pub use vec3::Vec3;
+
 pub const FILE_TYPE: &str = "P3";
 pub const MAX_VALUE: u32 = 255;
 pub const IMAGE_WIDTH: u32 = 256;
