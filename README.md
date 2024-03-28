ac1cae0315a67ef584e2ae8f0eaf2f8e4f84967c
rybak2Z <rybak2Z.000@gmail.com>
2024-03-28 12:43:56 +0100

    Implement outputting an image

--- README.md
+++ README.md
@@ -2,3 +2,5 @@
 
 This is my Rust implementation of Peter Shirley's 
 [Raytracing in One Weekend](https://raytracing.github.io/books/RayTracingInOneWeekend.html#metal) tutorial/book.
+
+Usage: `cargo run -r > out.ppm`
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,33 @@
-fn main() {
-    println!("Hello, world!");
+use std::io::{self, BufWriter, Write};
+
+const FILE_TYPE: &str = "P3";
+const MAX_VALUE: u32 = 255;
+const IMAGE_WIDTH: u32 = 256;
+const IMAGE_HEIGHT: u32 = 256;
+
+fn main() -> io::Result<()> {
+    let mut stdout = BufWriter::new(io::stdout());
+    write!(
+        stdout,
+        "{FILE_TYPE}\n{IMAGE_WIDTH} {IMAGE_HEIGHT}\n{MAX_VALUE}\n"
+    )?;
+
+    for row in 0..IMAGE_HEIGHT {
+        for col in 0..IMAGE_WIDTH {
+            let red = col as f32 / (IMAGE_WIDTH - 1) as f32;
+            let green = row as f32 / (IMAGE_HEIGHT - 1) as f32;
+            let blue = 0;
+
+            let red = (red * MAX_VALUE as f32) as u32;
+            let green = (green * MAX_VALUE as f32) as u32;
+
+            write!(stdout, "{red} {green} {blue} ")?;
+        }
+
+        writeln!(stdout)?;
+    }
+
+    stdout.flush()?;
+
+    Ok(())
 }
