e61d2c0aa4a7803f706891b9f91553fb6703c7ca
Felix Fischer <felixfischer0204@gmail.com>
2024-03-28 13:24:04 +0100

    Print progress

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use std::io::{self, BufWriter, Write};
+use std::time::Instant;
 
 const FILE_TYPE: &str = "P3";
 const MAX_VALUE: u32 = 255;
@@ -11,6 +12,8 @@ fn main() -> io::Result<()> {
         stdout,
         "{FILE_TYPE}\n{IMAGE_WIDTH} {IMAGE_HEIGHT}\n{MAX_VALUE}\n"
     )?;
+
+    let time_start = Instant::now();
 
     for row in 0..IMAGE_HEIGHT {
         for col in 0..IMAGE_WIDTH {
@@ -25,9 +28,30 @@ fn main() -> io::Result<()> {
         }
 
         writeln!(stdout)?;
+        print_progress(row);
     }
 
     stdout.flush()?;
-
+    print_finish(time_start);
     Ok(())
+}
+
+fn print_progress(row: u32) {
+    let progress = row as f32 / (IMAGE_HEIGHT - 1) as f32;
+    let lines_remaining = IMAGE_HEIGHT - (row + 1);
+    let cleaning = "     "; // Needed if the current output line is shorter than the line that gets overwritten
+    eprint!(
+        "\rProgress: {:.2} % (scanlines remaining: {}){}",
+        progress * 100.0,
+        lines_remaining,
+        cleaning
+    );
+}
+
+fn print_finish(time_start: Instant) {
+    let duration = time_start.elapsed();
+    let seconds = duration.as_secs();
+    let minutes = seconds / 60;
+    let rest_seconds = seconds % 60;
+    eprintln!("\nFinished after {:02}m{:02}s", minutes, rest_seconds);
 }
