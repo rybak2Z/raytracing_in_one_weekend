29c1cb560a879d23e95bdc09689321da22ecb03d
Felix Fischer <felixfischer0204@gmail.com>
2024-03-29 09:19:11 +0100

    Add Ray struct

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,11 @@
 pub mod color;
 pub mod point3;
+pub mod ray;
 pub mod vec3;
 
 pub use color::Color;
 pub use point3::Point3;
+pub use ray::Ray;
 pub use vec3::Vec3;
 
 pub const FILE_TYPE: &str = "P3";
