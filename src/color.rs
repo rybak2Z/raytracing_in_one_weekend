1946838421ebb4d5a4aea2ac2b9c232930114efd
rybak2Z <rybak2Z.000@gmail.com>
2024-03-30 14:32:45 +0100

    Simplify code and implement camera that renders background
    The 'simplifying' gets rid of abstractions that were annoying and likely useless for the future

--- src/color.rs
+++ src/color.rs
@@ -1,6 +1,8 @@
 #![allow(dead_code)]
 
-use crate::config::MAX_VALUE;
+use crate::MAX_VALUE;
+
+use std::ops::{Add, Div, DivAssign, Mul, MulAssign};
 
 #[derive(Debug, Clone, Copy)]
 pub struct Color {
@@ -26,5 +28,51 @@ impl Color {
             (self.g * factor) as u32,
             (self.b * factor) as u32,
         )
+    }
+}
+
+impl Add for Color {
+    type Output = Self;
+
+    fn add(self, rhs: Self) -> Self::Output {
+        Color::new(self.r + rhs.r, self.g + rhs.g, self.b + rhs.b)
+    }
+}
+
+impl Mul<f32> for Color {
+    type Output = Self;
+
+    fn mul(self, rhs: f32) -> Self::Output {
+        Color::new(self.r * rhs, self.g * rhs, self.b * rhs)
+    }
+}
+
+impl Mul<Color> for f32 {
+    type Output = Color;
+
+    fn mul(self, rhs: Color) -> Self::Output {
+        rhs * self
+    }
+}
+
+impl MulAssign<f32> for Color {
+    fn mul_assign(&mut self, rhs: f32) {
+        self.r *= rhs;
+        self.g *= rhs;
+        self.b *= rhs;
+    }
+}
+
+impl Div<f32> for Color {
+    type Output = Self;
+
+    fn div(self, rhs: f32) -> Self::Output {
+        self * (1.0 / rhs)
+    }
+}
+
+impl DivAssign<f32> for Color {
+    fn div_assign(&mut self, rhs: f32) {
+        *self *= 1.0 / rhs;
     }
 }
